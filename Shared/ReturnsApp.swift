2afb9802247d04a6c91dc4d7aef92107a518ed65
James Chen <james@ashchan.com>
2021-07-19 16:49:13 +0900

    Initial Commit

---
+++ Shared/ReturnsApp.swift
@@ -0,0 +1,20 @@
+//
+//  ReturnsApp.swift
+//  Shared
+//
+//  Created by James Chen on 2021/07/19.
+//
+
+import SwiftUI
+
+@main
+struct ReturnsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
