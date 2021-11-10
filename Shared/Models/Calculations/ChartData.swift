42ae4f3e50531f93d1df3efd3deec74695f91090
James Chen <james@ashchan.com>
2021-11-10 18:06:29 +0900

    Growth chart should show history returns (yet to implement), not balance changes

--- Shared/Models/Calculations/ChartData.swift
+++ Shared/Models/Calculations/ChartData.swift
@@ -15,6 +15,12 @@ struct ChartData {
         portfolio.sortedBalanceData.map { $0.balance.doubleValue }
     }
 
+    // TODO: calculate returns properly.
+    // FIXME: growth data entries should be based on history returns, not balance changes.
+    var growthData: [Double] {
+        portfolio.sortedBalanceData.map { $0.balance.doubleValue }
+    }
+
     // Account based most recent month balance values on close date (account name: account balance).
     var totalAssetsData: [String: Double] {
         portfolio.sortedAccounts.reduce(into: [String: Double]()) { result, account in
