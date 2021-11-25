249ee15ee3034c5cc84ffe006a63d6831752665e
James Chen <james@ashchan.com>
2021-11-26 08:42:00 +0900

    Show quick account help

--- macOS/Views/Sidebar/AccountRow.swift
+++ macOS/Views/Sidebar/AccountRow.swift
@@ -16,6 +16,7 @@ struct AccountRow: View {
     @ObservedObject var account: Account
     @State private var showingDeletePrompt = false
     @State private var showingRenameSheet = false
+    @State private var showingAccountHelpPopover = false
     @Binding var selection: String?
 
     var body: some View {
@@ -23,7 +24,20 @@ struct AccountRow: View {
             destination: AccountRecordList(account: account)
                 .navigationTitle(account.name ?? "")
                 .navigationSubtitle("Portfolio: \(portfolio.name ?? "")")
+                .toolbar {
+                    ToolbarItemGroup {
+                        Button {
+                            showingAccountHelpPopover.toggle()
+                        } label: {
+                            Label("Configure...", systemImage: "questionmark.circle")
+                        }
+                        .popover(isPresented: $showingAccountHelpPopover, arrowEdge: .bottom) {
+                            AccountHelpPopover()
+                        }
+                    }
+                }
                 .environmentObject(portfolioSettings),
+
             tag: account.tag,
             selection: $selection
         ) {
@@ -79,6 +93,26 @@ extension AccountRow {
                 print("Failed to save, error \(error)")
             }
         }
+    }
+}
+
+struct AccountHelpPopover: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("For each month, provide three pieces of data:")
+                .frame(width: 400, alignment: .leading)
+                .padding(5)
+            Text("**Total contributions.** \nMoney added to portfolio, excluding any internal cash flows such as dividend payments.")
+                .frame(width: 400, alignment: .leading)
+                .padding(5)
+            Text("**Total withdrawals.** \nMoney removed from portfolio, excluding any internal cash flows such as fees.")
+                .frame(width: 400, alignment: .leading)
+                .padding(5)
+            Text("**Portfolio balance** at the close of the last day of the month.")
+                .frame(width: 400, alignment: .leading)
+                .padding(5)
+        }
+        .padding()
     }
 }
 
