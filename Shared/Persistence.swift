e2c92530087c5220f9d724df333ac9cef827ebcc
James Chen <james@ashchan.com>
2021-11-28 17:40:31 +0900

    Not going to implement CloudKit sync

--- Shared/Persistence.swift
+++ Shared/Persistence.swift
@@ -26,10 +26,10 @@ struct PersistenceController {
         return result
     }()
 
-    let container: NSPersistentCloudKitContainer
+    let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentCloudKitContainer(name: "Returns")
+        container = NSPersistentContainer(name: "Returns")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
